f4a1e8ee835904dd6114970056b356678e1e97bb
JoHiu <80262385+JoHiu@users.noreply.github.com>
2021-06-14 19:20:01 +0700

    PunyaHeli

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,13 @@
 const isLogin = (req, res, next) => {
-  if (req.session.user == null || req.session.user == undefined) {
-    req.flash('alertMessage', 'Session telah habis silahkan signin kembali!!');
-    req.flash('alertStatus', 'danger');
-    res.redirect('/admin/signin');
-  } else {
-    next();
-  }
+    if(req.session.user == null || req.session.user == undefined){
+        req.flash('alertMessage', 'Session telah berakhir, silahkan sign in kembali');
+        req.flash('alertStatus', 'danger');
+        res.redirect('/admin/signin');
+    }
+
+    else{
+        next();
+    }
 }
 
 module.exports = isLogin;
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,10 +3,10 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: "public/images",
-  filename: function (req, file, cb) {
+  filename: function(req, file, cb) {
     cb(null, Date.now() + path.extname(file.originalname));
   }
-});
+})
 
 const uploadSingle = multer({
   storage: storage,
@@ -24,14 +24,21 @@ const uploadMultiple = multer({
   }
 }).array("image");
 
+// // Check file Type
 function checkFileType(file, cb) {
+
+  // Allowed ext
   const fileTypes = /jpeg|jpg|png|gif/;
+  // Check ext
   const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
   const mimeType = fileTypes.test(file.mimetype);
+
   if (mimeType && extName) {
     return cb(null, true);
   } else {
     cb("Error: Images Only !!!");
   }
 }
+
 module.exports = { uploadSingle, uploadMultiple };
